3f2486dbe35f6d400fb73e753a9581d0bc91c208
Berkay Yilmazer <byilmazer@stud.hs-heilbronn.de>
2024-01-20 22:53:02 +0100

    Update Tic Tac Toe UI and add player form

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -6,29 +6,41 @@ const ticTacToe = (function () {
   ];
 
   let players = {};
+  let currentPlayer = null;
+  const gameBoardElement = document.getElementById("board");
 
   const gameBoard = {
     getBoard: () => board,
     setBoard: (newBoard) => {
       board = newBoard;
     },
+    resetBoard: () => {
+      board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ];
+    },
   };
 
   function Player(player, symbol) {
+    console.log(currentPlayer);
     return { player, symbol };
   }
 
   const displayController = {
+    cell: null,
     displayBoard: () => {
       const board = gameBoard.getBoard();
       console.log(board);
       for (let x = 0; x < board.length; x++) {
         for (let y = 0; y < board[x].length; y++) {
-          const cell = document.getElementById(`cell${x}${y}`);
+          this.cell = document.getElementById(`cell${x}${y}`);
           cell.innerHTML = board[x][y] || ""; // Update the cell's content
           if (board[x][y] == null) {
             cell.onclick = () => {
-              gameController.makeMove(players["x"], [x, y]);
+              console.log(players[currentPlayer.symbol].symbol);
+              gameController.makeMove(players[currentPlayer.symbol], [x, y]);
             };
           } else {
             cell.onclick = null; // Remove the onclick event from filled cells
@@ -39,25 +51,67 @@ const ticTacToe = (function () {
   };
 
   const gameController = {
+    handleFormSubmission: function () {
+      document
+        .getElementById("player-form")
+        .addEventListener("submit", function (event) {
+          event.preventDefault(); // Prevent the form from submitting normally
+
+          const player1Name = document.getElementById("player1-name").value;
+          const player2Name = document.getElementById("player2-name").value;
+
+          const player1 = gameController.createPlayer(player1Name, "X");
+          const player2 = gameController.createPlayer(player2Name, "O");
+
+          // Set the current player
+          currentPlayer = player1;
+
+          gameBoardElement.style.backgroundColor = "#2196f3";
+          const gameStatus = document.getElementById("game-status");
+          gameStatus.innerHTML = "";
+
+          // You can now start the game
+          gameBoard.resetBoard();
+          displayController.displayBoard();
+        });
+    },
     createPlayer: (name, symbol) => {
       const player = Player(name, symbol);
+      currentPlayer = player;
       players[symbol] = player;
+      console.log(players);
       return player;
     },
     makeMove: (player, position) => {
       const [row, col] = position;
-      if (gameBoard.getBoard()[row][col] !== null) {
-        console.log("Please choose another field");
-        return;
-      }
       let newBoard = gameBoard.getBoard();
       newBoard[row][col] = player.symbol;
       gameBoard.setBoard(newBoard);
+      currentPlayer =
+        currentPlayer === players["X"] ? players["O"] : players["X"];
+
+      gameBoardElement.style.backgroundColor =
+        currentPlayer.symbol === "X" ? "#2196f3" : "#fa4343";
       displayController.displayBoard();
       gameController.checkWin();
     },
+    resetGame: () => {
+      const resetButton = document.getElementById("restart");
+
+      // Disable all game cells
+      const gameCells = document.querySelectorAll(".gameCell");
+      gameCells.forEach((cell) => (cell.onclick = null));
+      resetButton.onclick = () => {
+        gameBoard.resetBoard();
+        displayController.displayBoard();
+        resetButton.onclick = null;
+        const gameStatus = document.getElementById("game-status");
+        gameStatus.innerHTML = "";
+      };
+    },
     checkWin: () => {
       const board = gameBoard.getBoard();
+      const gameStatus = document.getElementById("game-status");
       const lines = [
         [board[0][0], board[0][1], board[0][2]],
         [board[1][0], board[1][1], board[1][2]],
@@ -69,14 +123,30 @@ const ticTacToe = (function () {
         [board[0][2], board[1][1], board[2][0]],
       ];
       for (let line of lines) {
-        if (line.every((value) => value === "x")) {
-          console.log(`Player: ${players["x"].player} won`);
-          return "x";
-        } else if (line.every((value) => value === "o")) {
-          console.log(`Player: ${players["o"].player} won`);
-          return "o";
+        if (line.every((value) => value === "X")) {
+          console.log(`Player: ${players["X"].player} won`);
+          gameStatus.innerHTML = `Player: ${players["X"].player} won`;
+          gameBoardElement.style.backgroundColor = "#2196f3";
+          gameController.resetGame();
+
+          return "X";
+        } else if (line.every((value) => value === "O")) {
+          console.log(`Player: ${players["O"].player} won`);
+          gameStatus.innerHTML = `Player: ${players["O"].player} won`;
+          gameBoardElement.style.backgroundColor = "#fa4343";
+          gameController.resetGame();
+
+          return "O";
+        } else if (board.every((row) => row.every((value) => value !== null))) {
+          console.log("Draw");
+          gameStatus.innerHTML = "Draw";
+          gameBoardElement.style.backgroundColor = "green";
+          gameController.resetGame();
+
+          return "draw";
         }
       }
+
       return null;
     },
   };
@@ -85,9 +155,6 @@ const ticTacToe = (function () {
 })();
 
 let game = ticTacToe;
-let player1 = game.gameController.createPlayer("berkay", "x");
-let player2 = game.gameController.createPlayer("max", "o");
+game.gameController.handleFormSubmission();
 
-console.log("sasS");
-game.displayController.displayBoard();
 //game.gameController.makeMove(player1, [0, 0]);
