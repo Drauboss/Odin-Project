a7f800f78b21aa2998b1332298bfe9ec113ef478
Berkay Yilmazer <byilmazer@stud.hs-heilbronn.de>
2024-02-02 22:01:09 +0100

    remove Giphy API key in script.js

--- API/script.js
+++ API/script.js
@@ -3,7 +3,7 @@ const input = document.querySelector("input");
 
 function getNewImage(searchTerm) {
   fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=632nhwNzkYC9MFQHEfLfnKzFL7ZSgmHw&s=${searchTerm}`,
+    `https://api.giphy.com/v1/gifs/translate?api_key=API_KEY&s=${searchTerm}`,
     {
       mode: "cors",
     }
